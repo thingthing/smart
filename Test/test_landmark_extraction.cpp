5b1849e9f9e7ccd0ec151bf0fcfabe8e81a2b8d1
jyb <tony.taieb@epitech.eu>
2014-11-06 18:44:48 -0800

    SLAM: adding new tests for Landmarks

--- Test/test_landmark_extraction.cpp
+++ Test/test_landmark_extraction.cpp
@@ -2,14 +2,22 @@
 #include <Landmarks.hh>
 
 // Add true landmarks class when ready
-namespace	Result
+namespace	Landmark_Result
 {
   int		id = -1;
   int		life = LIFE;
   int		totalTimesObserved = 0;
   double	range = -1;
   double	bearing = -1;
-  double	pos[2] = {0, 0};
+  double	pos[2] = {0.0, 0.0};
+};
+
+namespace	Landmarks_Result
+{
+  int		DBSize = 0;
+  int		EKFLandmarks = 0;
+  double	defaultdegreePerScan = 0.5;
+  double	degreePerScan = 4.2;
 };
 
 using namespace igloo;
@@ -18,14 +26,25 @@ When(creating_a_landmark)
 {
   Then(it_should_have_default_value)
     {
-      Assert::That(lm.id, Is().EqualTo(::Result::id));
-      Assert::That(lm.life, Is().EqualTo(::Result::life));
-      Assert::That(lm.totalTimeObserved, Is().EqualTo(::Result::totalTimesObserved));
-      Assert::That(lm.range, Is().EqualTo(::Result::range));
-      Assert::That(lm.bearing, Is().EqualTo(::Result::bearing));
-      Assert::That(lm.pos[0], Is().EqualTo(::Result::pos[0]));
-      Assert::That(lm.pos[1], Is().EqualTo(::Result::pos[1]));
+      Assert::That(lm.id, Is().EqualTo(::Landmark_Result::id));
+      Assert::That(lm.life, Is().EqualTo(::Landmark_Result::life));
+      Assert::That(lm.totalTimeObserved, Is().EqualTo(::Landmark_Result::totalTimesObserved));
+      Assert::That(lm.range, Is().EqualTo(::Landmark_Result::range));
+      Assert::That(lm.bearing, Is().EqualTo(::Landmark_Result::bearing));
+      Assert::That(lm.pos[0], Is().EqualTo(::Landmark_Result::pos[0]));
+      Assert::That(lm.pos[1], Is().EqualTo(::Landmark_Result::pos[1]));
     }
 
   ::Landmarks::Landmark	lm;
+};
+
+When(creating_Landmarks)
+{
+  Then(should_have_default_landmarks_value)
+  {
+    Assert::That(lms.getDBSize(), Is().EqualTo(::Landmarks_Result::DBSize));
+    Assert::That(lms.degreePerScan, Is().EqualTo(::Landmarks_Result::defaultdegreePerScan));
+  }
+
+  Landmarks lms;
 };
