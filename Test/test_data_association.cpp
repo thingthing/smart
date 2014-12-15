d5438509b92669449ee08ca90fadd2a818291498
THING-LEOH Nicolas <nicolas1.thing-leoh@epitech.eu>
2014-12-15 02:04:25 -0500

    SLAM: Test: associateLandmarks()

--- Test/test_data_association.cpp
+++ Test/test_data_association.cpp
@@ -38,3 +38,82 @@ When(getting_db_from_data_association)
   ::DataAssociation datas;
   ::Landmarks *landmarks;
 };
+
+/*
+ * Test landmark association
+ */
+When(associating_landmarks)
+{
+  ScenarioAttribute("hasChild", "true")
+
+  void SetUp()
+    {
+      landmarks = new Landmarks();
+      datas = new DataAssociation(landmarks);
+      to_test = new Landmarks::Landmark();
+      id = to_test->id;
+      totalTimeObserved = to_test->totalTimeObserved;
+    }
+
+  When(landmarks_db_is_empty)
+  {
+    ScenarioAttribute("hasParent", "\t")
+
+      void SetUp()
+      {
+	Root().result = Root().datas->associateLandmarks(Root().to_test);
+      }
+
+    Then(it_should_return_false)
+    {
+      AssertThatDetail(Root().result, Is().EqualTo(false));
+    }
+
+    Then(it_should_not_change_landmark_data)
+    {
+      AssertThatDetail(Root().to_test->id, Is().EqualTo(Root().id));
+      AssertThatDetail(Root().to_test->totalTimeObserved,
+		   Is().EqualTo(Root().totalTimeObserved));
+    }
+  };
+
+  When(landmarks_db_is_not_empty)
+  {
+    ScenarioAttribute("hasParent", "\t")
+
+      void SetUp()
+      {
+	Root().new_id = Root().landmarks->addToDB(*Root().to_test);
+	Root().landmarks->landmarkDB[Root().new_id]->totalTimeObserved =
+	  Landmarks::MINOBSERVATIONS + 1;
+	Root().result = Root().datas->associateLandmarks(Root().to_test);
+      }
+
+    Then(it_should_return_true)
+    {
+      AssertThatDetail(Root().result, Is().EqualTo(true));
+    }
+
+    Then(it_should_change_landmark_data)
+    {
+      AssertThatDetail(Root().to_test->id, Is().Not().EqualTo(Root().id));
+      AssertThatDetail(Root().to_test->totalTimeObserved,
+		   Is().Not().EqualTo(Root().totalTimeObserved));
+    }
+
+    Then(it_should_set_good_landmark_data)
+    {
+      AssertThatDetail(Root().to_test->id, Is().EqualTo(Root().new_id));
+      AssertThatDetail(Root().to_test->totalTimeObserved,
+		   Is().EqualTo(Landmarks::MINOBSERVATIONS + 1));
+    }
+  };
+
+  ::DataAssociation *datas;
+  ::Landmarks *landmarks;
+  ::Landmarks::Landmark *to_test;
+  bool	result;
+  int	id;
+  int	totalTimeObserved;
+  int	new_id;
+};
