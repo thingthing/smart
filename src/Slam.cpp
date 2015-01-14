cb82bfd60fe4906ee44a392666344986b2ff0136
THING-LEOH Nicolas <nicolas1.thing-leoh@epitech.eu>
2015-01-14 03:23:45 -0500

    SLAM: Slam class constructor and destrcutor

---
+++ src/Slam.cpp
@@ -0,0 +1,32 @@
+#include "Slam.hh"
+
+Slam::Slam(Agent *agent)
+{
+  this->_agent = agent;
+  this->_landmarkDb = new Landmarks(agent->degreePerScan);
+  this->_data = new DataAssociation(this->_landmarkDb);
+  this->_state = new SystemStateMatrice(agent->getPos().x, agent->getPos().y, agent->getBearing());
+  this->_covariance = new CovarianceMatrice(agent->getPos().x, agent->getPos().y, agent->getBearing());
+}
+
+Slam::~Slam()
+{
+  if (this->_data)
+    delete this->_data;
+  if (this->_landmarkDb)
+    delete this->_landmarkDb;
+  if (this->_state)
+    delete this->_state;
+  if (this->_covariance)
+    delete this->_covariance;
+}
+
+// void		Slam::updateState(pcl::PointXYZ cameradata[], int numberSample)
+// {
+
+// }
+
+// void		Slam::addLandmarks(pcl::PointXYZ cameradata[], int numberSample)
+// {
+
+// }
